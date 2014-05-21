4e305d3e4fb5a01335cc9a2a42ae67ecc5b8abab
Anton Novoselov <novic.dev@gmail.com>
2014-05-21 16:13:45 +0400

    ActivitiesFragment: activity history messages localization

--- src/com/seafile/seadroid2/data/SeafActivity.java
+++ src/com/seafile/seadroid2/data/SeafActivity.java
@@ -1,11 +1,15 @@
 package com.seafile.seadroid2.data;
 
 import java.util.Date;
+import java.util.StringTokenizer;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Context;
+
 import com.seafile.seadroid2.R;
+import com.seafile.seadroid2.SeadroidApplication;
 
 /**
  * SeafActivity
@@ -20,12 +24,68 @@ public class SeafActivity implements ActivityItem {
 	public String user;
 	public String mtime; // the last modification time
 
+	private static Context context = SeadroidApplication.getAppContext();
+
 	static SeafActivity fromJson(JSONObject obj) {
 		SeafActivity act = new SeafActivity();
 		try {
 			act.id = obj.getString("repo_id");
 			act.name = obj.getString("repo_name");
-			act.description = obj.getString("desc");
+
+			/**
+			 * Split and localize the Description text TODO:
+			 */
+			StringTokenizer tokens = new StringTokenizer(obj.getString("desc"),
+					" ");
+			String action = tokens.nextToken();
+			String objname = tokens.nextToken();
+
+			if (action.equalsIgnoreCase("Added")) {
+				if (objname.equalsIgnoreCase("directory")) {
+					action = context.getResources().getString(
+							R.string.action_dir_added);
+					objname = context.getResources().getString(
+							R.string.action_dir);
+				} else {
+					action = context.getResources().getString(
+							R.string.action_added);
+				}
+
+			} else if (action.equalsIgnoreCase("Removed")) {
+				if (objname.equalsIgnoreCase("directory")) {
+					action = context.getResources().getString(
+							R.string.action_dir_removed);
+					objname = context.getResources().getString(
+							R.string.action_dir);
+				} else {
+					action = context.getResources().getString(
+							R.string.action_removed);
+				}
+
+			} else if (action.equalsIgnoreCase("Renamed")) {
+				if (objname.equalsIgnoreCase("directory")) {
+					action = context.getResources().getString(
+							R.string.action_dir_renamed);
+					objname = context.getResources().getString(
+							R.string.action_dir);
+				} else {
+					action = context.getResources().getString(
+							R.string.action_renamed);
+				}
+
+			} else if (action.equalsIgnoreCase("Changed")) {
+				if (objname.equalsIgnoreCase("directory")) {
+					action = context.getResources().getString(
+							R.string.action_dir_changed);
+					objname = context.getResources().getString(
+							R.string.action_dir);
+				} else {
+					action = context.getResources().getString(
+							R.string.action_changed);
+				}
+			}
+
+			act.description = action + " " + objname;
 			act.user = obj.getString("nick");
 			long mt = obj.getLong("time");
 			act.mtime = new Date(mt).toLocaleString();
